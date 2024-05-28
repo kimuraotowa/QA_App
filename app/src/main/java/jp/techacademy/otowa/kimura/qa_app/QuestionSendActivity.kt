32efc73e102c102204096f7ee32bc893f2d3b6cf
木村乙羽 <o-kimura@i-enter.co.jp>
2024-05-28 15:26:24 +0900

    Lesson8 Chapter7 → 画像を作る

---
+++ app/src/main/java/jp/techacademy/otowa/kimura/qa_app/QuestionSendActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.otowa.kimura.qa_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionSendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_send)
+    }
+}
